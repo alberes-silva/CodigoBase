d388754fc28365fd2d7de346deda39da3a95db05
Alberes Silva <alberessilva@yahoo.com.br>
2025-02-05 12:15:40 -0300

    Create OlaMundo.java

---
+++ Java/OlaMundo/src/OlaMundo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Olá Mundo!");
+		
+		Scanner input = new Scanner(System.in);
+		int idade;
+		Scanner input2 = new Scanner(System.in);
+		String nome;
+		
+		System.out.println("Qual é o seu nome");
+		nome = input2.nextLine();
+		System.out.println("Qual a sua idade? ");
+		idade = input.nextInt();
+		
+		
+		System.out.println("O meu nome é "+ nome);
+		System.out.println("A minha idade é "+idade);
+		
+		
+		
+	}
+
+}
