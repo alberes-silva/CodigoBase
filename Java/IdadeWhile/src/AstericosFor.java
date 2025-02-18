6deb84347252d87f4b7b2372c8f5ce54c15daa15
alberes <alberessilva@yahoo.com.br>
2025-02-18 12:29:21 -0300

    Create AstericosFor.java

---
+++ Java/IdadeWhile/src/AstericosFor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class AstericosFor {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for(int i = 1; i <= 10; i++) {
+			for(int j = 1; j <= 10; j++) {
+				System.out.print("*");
+			}
+				System.out.println();
+		}
+
+	}
+
+}
