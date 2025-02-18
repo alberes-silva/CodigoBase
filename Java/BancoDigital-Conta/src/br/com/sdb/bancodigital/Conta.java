fc8f4e662c8c4ef6202618a606a014c8f47814e7
alberes <alberessilva@yahoo.com.br>
2025-02-18 10:59:27 -0300

    Create Conta.java

---
+++ Java/BancoDigital-Conta/src/br/com/sdb/bancodigital/Conta.java
@@ -0,0 +1,29 @@
+package br.com.sdb.bancodigital;
+
+public class Conta {
+	
+	private int numero;
+	private double saldo;
+	
+	public Conta( int numero) {
+		this.numero = numero;
+		this.saldo = 0.0;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void depositar(double valor) {
+		if(valor > 0)
+		this.saldo += valor;
+	}
+	
+	
+
+}
