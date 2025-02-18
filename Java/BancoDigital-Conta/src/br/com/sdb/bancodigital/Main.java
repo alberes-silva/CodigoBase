28df889608e224350dad60bf5af4350710d7e022
alberes <alberessilva@yahoo.com.br>
2025-02-18 10:59:30 -0300

    Create Main.java

---
+++ Java/BancoDigital-Conta/src/br/com/sdb/bancodigital/Main.java
@@ -0,0 +1,21 @@
+package br.com.sdb.bancodigital;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Conta contaCorrente = new Conta(123456);
+		contaCorrente.depositar(1000);
+		
+		Conta contaPoupanca = new Conta(654321);
+		contaPoupanca.depositar(500);
+		
+		System.out.println("Conta: "+contaCorrente.getNumero()+
+				" possui o saldo de R$"+contaCorrente.getSaldo());
+		
+		System.out.println("Conta: "+contaPoupanca.getNumero()+
+				" possui o saldo de R$"+contaPoupanca.getSaldo());
+
+	}
+
+}
