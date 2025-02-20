2496ffa3fadc866ff7b59182523d7da67428a9df
alberes <alberessilva@yahoo.com.br>
2025-02-20 09:19:39 -0300

    Create PDFTextStripper.java

---
+++ Java/PDFReader/src/org/apache/pdfbox/text/PDFTextStripper.java
@@ -0,0 +1,12 @@
+package org.apache.pdfbox.text;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+public class PDFTextStripper {
+
+	public String getText(PDDocument document) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
