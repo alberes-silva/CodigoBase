8c5d652d484fcd92a80ce968c6a586d1a5003b69
alberes <alberessilva@yahoo.com.br>
2025-02-20 09:19:34 -0300

    Create PDDocument.java

---
+++ Java/PDFReader/src/org/apache/pdfbox/pdmodel/PDDocument.java
@@ -0,0 +1,17 @@
+package org.apache.pdfbox.pdmodel;
+
+import java.io.File;
+
+public class PDDocument {
+
+	public static PDDocument load(File file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
