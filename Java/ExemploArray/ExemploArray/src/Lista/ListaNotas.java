5e0edfd658c4685ee1caa339f1b51d43bf532df2
alberes <alberessilva@yahoo.com.br>
2025-02-21 10:41:34 -0300

    Create ListaNotas.java

---
+++ Java/ExemploArray/ExemploArray/src/Lista/ListaNotas.java
@@ -0,0 +1,14 @@
+package Lista;
+
+import java.util.ArrayList;
+
+public class ListaNotas {
+
+	public static void main(String[] args) {
+
+
+		ArrayList<Double> listaDeNotas = new ArrayList<>();
+		
+	}
+
+}
