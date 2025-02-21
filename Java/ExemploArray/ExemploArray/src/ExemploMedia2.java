c53333824b593f7de3049afaaf525572ca9361e6
alberes <alberessilva@yahoo.com.br>
2025-02-21 10:41:17 -0300

    Create ExemploMedia2.java

---
+++ Java/ExemploArray/ExemploArray/src/ExemploMedia2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ExemploMedia2 {
+
+	public static void main(String[] args) {
+	
+		double[] notas = {5.5, 7.5, 6, 8.5};
+		
+		double media = 0;
+		double soma = 0;
+		for(int i = 0; i < notas.length;i++) {
+
+			media = (soma += notas[i]) / notas.length;
+			
+		} 
+		System.out.println(media);
+	}
+
+}
