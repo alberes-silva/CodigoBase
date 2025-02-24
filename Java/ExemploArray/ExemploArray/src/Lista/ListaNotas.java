4a4723652ef1267e1a7eae1d1ef8cea1e190c23c
alberes <alberessilva@yahoo.com.br>
2025-02-24 10:39:32 -0300

    Update ListaNotas.java

--- Java/ExemploArray/ExemploArray/src/Lista/ListaNotas.java
+++ Java/ExemploArray/ExemploArray/src/Lista/ListaNotas.java
@@ -7,7 +7,18 @@ public class ListaNotas {
 	public static void main(String[] args) {
 
 
-		ArrayList<Double> listaDeNotas = new ArrayList<>();
+		ArrayList<Integer> listaDeNotas = new ArrayList<>();
+		
+		listaDeNotas.add(8);
+		listaDeNotas.add(2);
+		listaDeNotas.add(10);
+		listaDeNotas.add(5);
+		listaDeNotas.add(3);
+		
+		//foreach
+		for(Integer nota: listaDeNotas){
+			System.out.println(nota);
+		}
 		
 	}
 
